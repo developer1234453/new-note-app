a8a37e4b172145aa2db007e02ed861d7a179075d
midhun A <158081513+developer1234453@users.noreply.github.com>
2024-07-18 22:27:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 
 
 // MongoDB connection
-const db = "mongodb://localhost:127.0.1/NEW-NOTES-PACK";  
+const db = "mongodb://localhost:27017/NEW-NOTES-PACK";  
 mongoose.connect(db)
   .then(() =>{ 
     console.log('MongoDB connected')
@@ -32,5 +32,5 @@ mongoose.connect(db)
 })
   .catch(err => {
     console.log(err);
-  
+  process.exit(1);
 })
