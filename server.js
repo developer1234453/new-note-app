d1249db6a1847dc659ff510995b7211393adb2bc
midhun A <158081513+developer1234453@users.noreply.github.com>
2024-07-18 22:56:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/NEW-NOTES-PACK';
 
 // MongoDB connection
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(MONGODB_URI)
 .then(() => {
   console.log('MongoDB connected');
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
