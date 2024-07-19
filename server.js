5f39be88c85053b32b105d6fb879097034e17214
midhun A <158081513+developer1234453@users.noreply.github.com>
2024-07-19 09:54:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/NEW-NOTES-PACK';
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/NEW-NOTES-PACK';
 
 // MongoDB connection
 mongoose.connect(MONGODB_URI)
